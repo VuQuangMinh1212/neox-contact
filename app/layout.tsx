c310b3b99827bfe8ac5b049f99a6391afbd88884
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-06-18 11:48:49 +0700

    change layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,30 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-const inter = Inter({ subsets: ["latin"] })
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NEOX Contact Card",
-  description: "Contact information for NEOX Company",
-    generator: 'v0.dev'
-}
+  title: "Nguyễn Hoàng Mai",
+  description: "Contact information",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <main className="min-h-screen bg-zinc-600">
           <div className="max-w-md mx-auto pb-8">
-            <nav className="flex justify-center gap-4 py-4">
-            </nav>
+            <nav className="flex justify-center gap-4 py-4"></nav>
             {children}
           </div>
         </main>
       </body>
     </html>
-  )
+  );
 }
